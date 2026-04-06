7be63ce423f10b656ffde4cbb5478b22283f97a7
DanielVega825 <danielvegalinares@hotmail.com>
2026-04-06 14:17:00 -0500

    Se agrega objeto 'letras'

--- main.js
+++ main.js
@@ -2,12 +2,132 @@ const letras = [
     {
         letra: "A",
         palabra: "Avion",
-        imagen: './assets/img/avion'
+        imagen: './assets/img/avion.png'
     },
     {
-        letra: "A",
-        palabra: "Avion",
-        imagen: './assets/img/avion.png'
+        letra: "B",
+        palabra: "Balon",
+        imagen: './assets/img/balon.png'
+    },
+    {
+        letra: "C",
+        palabra: "Café",
+        imagen: './assets/img/cafe.png'
+    },
+    {
+        letra: "D",
+        palabra: "Durazno",
+        imagen: './assets/img/durazno.png'
+    },
+    {
+        letra: "E",
+        palabra: "Erizo",
+        imagen: './assets/img/erizo.png'
+    },
+    {
+        letra: "F",
+        palabra: "Foca",
+        imagen: './assets/img/foca.png'
+    },
+    {
+        letra: "G",
+        palabra: "Galleta",
+        imagen: './assets/img/galleta.png'
+    },
+    {
+        letra: "H",
+        palabra: "Hipopotamo",
+        imagen: './assets/img/hipopotamo.png'
+    },
+    {
+        letra: "I",
+        palabra: "Iman",
+        imagen: './assets/img/Iman.png'
+    },
+    {
+        letra: "J",
+        palabra: "Jabon",
+        imagen: './assets/img/jabon.png'
+    },
+    {
+        letra: "K",
+        palabra: "Koala",
+        imagen: './assets/img/koala.png'
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
+    },
+    {
+        letra: "",
+        palabra: "",
+        imagen: ''
     },
     
 ]
