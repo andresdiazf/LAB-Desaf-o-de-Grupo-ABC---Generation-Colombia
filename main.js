316622a6eed98dba99c9b3e66e03a5b28dc4e862
DanielVega825 <danielvegalinares@hotmail.com>
2026-04-06 12:40:58 -0500

    se agregan documentos img y main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const letras = [
+    {
+        letra: "A",
+        palabra: "Avion",
+        imagen: './assets/img/avion'
+    },
+    {
+        letra: "A",
+        palabra: "Avion",
+        imagen: './assets/img/avion.png'
+    },
+    
+]
